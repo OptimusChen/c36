08f32a38c2cbba8a3257b9257c02c15e21e97cd9
OptimusChen <69561711+OptimusChen@users.noreply.github.com>
2020-11-19 19:00:22 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var canvas, backgroundImg, playerCount, database, form, player, game;
+var gameState = 0;
+var allPlayers;
+
+function setup(){
+  canvas = createCanvas(400, 400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  background("white");
+  if(playerCount === 4){
+    game.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    game.play();
+}
+}
